26c9ccca52e062c2e5522d396b5c008e9467266b
Bilguun Batchuluun <23LP6327@G304-CC0073.local>
2023-11-22 19:52:10 +0800

    update

--- DomEasy/script.js
+++ DomEasy/script.js
@@ -30,8 +30,41 @@ changeButton.addEventListener("click", updateText);
 
 // Create and Append Elements:
 // Use document.createElement() to create a new HTML element.
+let newText1 = document.createElement("p");
+
+// console.log(newText1);
+newText1.textContent = "This will be new text for paragragh";
+var container = document.querySelector(".container");
+container.appendChild(newText1);
+
 // Use appendChild() to append the new element to an existing one.
+let newParagraph = document.createElement("p");
+newParagraph.textContent = "New Paragraph";
+
+let existingParagraph = document.getElementById("text3");
+existingParagraph.appendChild(newParagraph);
+console.log(existingParagraph);
 // Toggle Visibility:
+function show() {
+  document.getElementById("text1").style.visibility = "hidden";
+}
+const hiddenText = document.getElementById("btn1");
+hiddenText.addEventListener("click", show);
+
 // Use document.getElementById() to get an element.
 // Toggle its visibility by changing the style.display property.
+function toggleVisibility() {
+  let invisibility = document.getElementById("text5");
+  // invisibility.style.display = "none";
+  // console.log(toggleVisibility);
+
+  if (invisibility.style.display == "none") {
+    invisibility.style.display = "block";
+  } else {
+    invisibility.style.display = "none";
+  }
+}
+let buttonVisible = document.getElementById("btn2");
+buttonVisible.addEventListener("click", toggleVisibility);
+
 //  like 2
---
+++ DomEasy/scriptloop.js
@@ -0,0 +1,10 @@
+// Create a List:
+// Use a loop (e.g., for or while) to generate a list of items dynamically and append them to an unordered list (<ul>).
+
+// Highlight Even/Odd Rows:
+// Use a loop to iterate over table rows and apply different styles to even and odd rows.
+// Dynamic Dropdown Options:
+// Use a loop to populate a dropdown (<select>) with dynamic options.
+// Delayed Content:
+// Use a loop with a setTimeout to create a delayed effect, such as displaying messages one at a time.
+// <https://teams.microsoft.com/l/message/19:8303e738354d489f9ea17c9a8d9e7143@thread.tacv2/1700562190326?tenantId=aae8fbe2-488b-49d9-b471-e4be61674a71&amp;groupId=446cc906-f90e-4693-8475-7e5eeaa73af4&amp;parentMessageId=1700562190326&amp;teamName=C23-LPD-24&amp;channelName=Lesson code 👨🏼‍💻&amp;createdTime=1700562190326&amp;allowXTenantAccess=false>
